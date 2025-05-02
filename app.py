d6b9446110481905fc043a41843a3b2e8b19df1d
Gaurav Singh <154744601+Gauravsingh564@users.noreply.github.com>
2025-05-03 03:14:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from PIL import Image
 HERE = os.path.dirname(__file__)
 
 # ─── 2. (Optional) Debug listing ────────────────────────────────────
-st.write("App folder:",os.getcwd())
+st.write("Signature_Vaification_App:",os.getcwd())
 st.write("Contents:", os.listdir(HERE))
 # ─── 1. Define your checkpoint location ────────────────────────────────
 
